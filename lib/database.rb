726a027fd00dbd958e105ff0cb4ed064b4bc5cdc
jackson-lafrance <jackson@codeflight.dev>
2026-09-03 21:32:13 -0400

    whoops

--- lib/database.rb
+++ lib/database.rb
@@ -6,7 +6,7 @@ module Database
   def init_database
     @db = SQLite3::Database.new("data.db")
     @db.results_as_hash = true
-    @db.execute "CREATE TABLE IF NOT EXISTS Events(id INTEGER NOT NULL PRIMARY KEY, color INTEGER, title TEXT NOT NULL, description TEXT, start_date TEXT, length INTEGER,
+    @db.execute "CREATE TABLE IF NOT EXISTS Bookings(id INTEGER NOT NULL PRIMARY KEY, color INTEGER, title TEXT NOT NULL, description TEXT, start_date TEXT, length INTEGER,
 times INTEGER, type TEXT)"
     @db.execute "CREATE TABLE IF NOT EXISTS Tasks(id INTEGER NOT NULL PRIMARY KEY, color INTEGER, title TEXT NOT NULL, description TEXT, due_date TEXT)"
   end
@@ -21,9 +21,9 @@ times INTEGER, type TEXT)"
     @db.execute "SELECT * FROM Tasks"
   end
 
-  def get_events
+  def get_bookings
     check_connection
-    @db.execute "SELECT * FROM Events"
+    @db.execute "SELECT * FROM Bookings"
   end
 
   def make_task(task)
@@ -32,10 +32,10 @@ times INTEGER, type TEXT)"
       task[:title], task[:description], task[:color], task[:start_date], task[:length], task[:times], task[:type]
   end
 
-  def make_event(event)
+  def make_booking(booking)
     check_connection
-    db.execute "INSERT INTO events (title, description, color, due_date) VALUES (?, ?, ?, ?)",
-      event[:title], event[:description], event[:color], event[:due_date]
+    db.execute "INSERT INTO Bookings (title, description, color, due_date) VALUES (?, ?, ?, ?)",
+      booking[:title], booking[:description], booking[:color], booking[:due_date]
   end
 
   private
